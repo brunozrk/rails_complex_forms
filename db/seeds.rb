17e1dac9dae43e58b8fe4707e9c8858907a856f5
Bruno Zeraik <brunozrk@gmail.com>
2015-04-25 22:20:07 -0300

    populate database for sales example

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Seller.create(name: 'seller 1')
+Seller.create(name: 'seller 2')
+
+Product.create(name: 'product 1')
+Product.create(name: 'product 2')
