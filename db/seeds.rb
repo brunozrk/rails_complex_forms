0ecbeb3fc5760546caac5d3841b0462a0c2a5efe
Bruno Zeraik <brunozrk@gmail.com>
2015-04-26 12:41:40 -0300

    populate database for games example

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,7 @@ Seller.create(name: 'seller 2')
 
 Product.create(name: 'product 1')
 Product.create(name: 'product 2')
+
+Player.create(name: 'player 1')
+Player.create(name: 'player 2')
+Player.create(name: 'player 3')
